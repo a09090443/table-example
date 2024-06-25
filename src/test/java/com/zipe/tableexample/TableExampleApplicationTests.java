8d0f931b3b3215ca26153650fe65b5a755cb36a7
Gary <zipe.daden@gmail.com>
2024-06-25 23:06:37 +0800

    Initial commit

---
+++ src/test/java/com/zipe/tableexample/TableExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipe.tableexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
